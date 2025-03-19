1e720f6bcc65f1e3a1e1acf8a89a34ac430bff07
Симпсон <mag_007@example.com>
2025-03-19 05:08:32 +0400

    Second commit: added recursive Fibonacci function starting with (1, 1, ...)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int fib(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << fib(10) << std::endl;  // Вывод: 55
     return 0;
 }
