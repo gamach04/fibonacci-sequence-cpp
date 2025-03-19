d770a448715d909b804c5c57f1399540e405ec59
Симпсон <mag_007@example.com>
2025-03-19 05:08:57 +0400

    Third commit: fixed Fibonacci sequence to start with (0, 1, ...)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 int fib(int n) {
-    if (n <= 1) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
         return 1;
     }
     return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
-    std::cout << fib(10) << std::endl;  // Вывод: 55
+    std::cout << fib(10) << std::endl;  // Вывод: 34
     return 0;
 }
